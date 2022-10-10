9202c935004a41f90c1a62a6d5f88f6412ab8044
SaiyanPrideKakarot <rohanbawane.89@gmail.com>
2022-10-10 16:38:08 +0530

    started

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const router = require('./routes/route')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect("", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDB is Conneced"))
+    .catch(error => console.log(error))
+
+app.use('/', router)
+
+app.use(function (req, res) {
+    return res.status(404).send({ status: false, message: "Url not found" })
+})
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express app is running on port " + (process.env.PORT || 3000))
+})
